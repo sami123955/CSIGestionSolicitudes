790f231dadbf62e971537f44eebd752a07ffc7ae
Daniel Mazo Alarcon <dmazo@conocimientocorporativo.com>
2017-01-27 09:22:24 -0500

    Se agregan metodos de sucursal post y get

--- src/app/controllers/Sucursal.component.ts
+++ src/app/controllers/Sucursal.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Sucursal } from '../models/Sucursal';
+import { DatosServidor } from '../models/DatosServidor';
+import { SucursalService } from '../services/Sucursal.service';
 
 @Component({
     selector: 'Sucursal',
     templateUrl: '../views/Sucursal.component.html',
     styleUrls: ['../../assets/css/Maestras.css'],
-    providers: []
+    providers: [SucursalService]
 })
 
 export class SucursalComponent implements OnInit {
@@ -14,8 +16,38 @@ export class SucursalComponent implements OnInit {
     model = new Sucursal('','','','','');
 
 
+    DatosServidor = new DatosServidor();
+
+
+    constructor (private _SucursalService: SucursalService) {}
+
+
     ngOnInit() {
-        //alert();
+        this.BuscarSucursal();
+    }
+
+    GuardarSucursal() {
+
+
+            this._SucursalService.GuardarSucursal(this.model, this.DatosServidor.url)
+            .subscribe(
+                data => alert(JSON.stringify(data)),
+                error => alert(error),
+                () => console.log('termina')
+            );
+
+    }
+
+    
+    BuscarSucursal(){
+        
+        this._SucursalService.BuscarSucursal(this.DatosServidor.url).subscribe(
+            data => alert(JSON.stringify(data)),
+            error => alert(error),
+            () => console.log('Termina')
+        );
+
+
     }
 
 }
---
+++ src/app/services/Sucursal.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Headers } from '@angular/http';
+import { RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SucursalService {
+    constructor(private _http: Http) { }
+
+    GuardarSucursal(SucursalObject: any, urlService: string) {
+
+        
+        var params = '?Direccion='+SucursalObject.Direccion+'&Nombre='+SucursalObject.Nombre+'&Email='+SucursalObject.Email+'&Telefono='+SucursalObject.Telefono+'&Representante='+SucursalObject.Representante+'&Estado='+true+'&CodigoEmpresa=';
+
+        var headers = new Headers();
+        
+        var options = new RequestOptions({ headers: headers });
+
+        return this._http.post(urlService + 'Sucursal'+ params
+            , '', options)
+            .map(res => res.json());
+
+    }
+
+    BuscarSucursal(UrlApi) {
+
+        var params = 'Direccion=&Nombre=&Email=&Telefono=&Representante=&Estado=&CodigoEmpresa=';
+
+        return this._http.get(UrlApi + 'Sucursal?' + params).map(res => res.json());
+    }
+
+
+    ActualizarEmpresa(empresaObject: any, urlService: string){
+        var frmData= empresaObject.Archivos;
+
+        
+        var params = '?Codigo='+empresaObject.Codigo+'&Nit='+empresaObject.Nit+'&RazonSocial='+empresaObject.RazonSocial+'&Direccion='+empresaObject.Direccion+'&DireccionRecepcion='+empresaObject.DireccionRecepcion+'&EmailEmpresa='+empresaObject.EmailEmpresa+'&Telefono='+empresaObject.Telefono+'&Representante='+empresaObject.Representante+'&Observaciones='+empresaObject.Observaciones+'&Estado='+empresaObject.Estado+'&Contacto='+empresaObject.Contacto+'&EmailContacto='+empresaObject.EmailContacto+'&RutaRut='+encodeURIComponent(empresaObject.RutaRut)+'&RutaCamaraComercio='+encodeURIComponent(empresaObject.RutaCamaraComercio)+'&Contrato='+encodeURIComponent(empresaObject.Contrato);
+
+        var headers = new Headers();
+
+        var options = new RequestOptions({ headers: headers });
+
+
+        return this._http.post(urlService + 'Empresa' + params
+            , frmData, options).map(res => res.json());
+
+    }
+
+
+
+}
